b62f143ab12e6b72ed8d1e40a891df7b5b561d2e
h28a3 <at.h.s_24328@iCloud.com>
2024-09-10 11:10:14 +0900

    Add files via upload

---
+++ header.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define N 5
+#define M 15
+#define BINGO_SIZE 5
+
+int src[N][M], bingo[BINGO_SIZE][BINGO_SIZE];
+
+void Initialization(void);
+
+void print_bingo(int count);
